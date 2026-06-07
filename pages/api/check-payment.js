3cc8dbb5ff6a8cb68071a36c57d353515e25c42e
Kacperek123123 <93077752+Kacperek123123@users.noreply.github.com>
2026-06-07 20:19:31 +0200

    Create check-payment.js

---
+++ pages/api/check-payment.js
@@ -0,0 +1,11 @@
+// Kod, który sprawdza blockchain (zamiast zewnętrznej bramki)
+export default async function handler(req, res) {
+  const { order_id, address } = req.body;
+  
+  // Używamy darmowego API Blockchair (brak KYC!)
+  const response = await fetch(`https://api.blockchair.com/litecoin/dashboards/address/${address}`);
+  const data = await response.json();
+  
+  // Sprawdzamy czy transakcja z odpowiednią kwotą/id istnieje
+  // Jeśli tak -> wywołujemy kod przyznający dostęp do bazy (jak w poprzednim pliku)
+}
