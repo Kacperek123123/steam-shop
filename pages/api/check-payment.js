44e25f950bde7354db67998c8c513ea9a3aca4e0
Kacperek123123 <93077752+Kacperek123123@users.noreply.github.com>
2026-06-07 20:20:37 +0200

    Update check-payment.js

--- pages/api/check-payment.js
+++ pages/api/check-payment.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   const { order_id, address } = req.body;
   
   // Używamy darmowego API Blockchair (brak KYC!)
-  const response = await fetch(`https://api.blockchair.com/litecoin/dashboards/address/${address}`);
+  const response = await fetch(`https://blockchair.com/litecoin/address/LM3eUhktfk69fRLXncjrRA4qEyULJmbbPc`);
   const data = await response.json();
   
   // Sprawdzamy czy transakcja z odpowiednią kwotą/id istnieje
